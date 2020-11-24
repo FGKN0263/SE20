5dc6a32d8a37a06a1d420c1c738f6499a77caed2
Kosei Nagasawa <f.g.mochiguma@gmail.com>
2020-11-24 21:12:39 +0900

    update Sample.kt

--- 201111/Sample.kt
+++ 201111/Sample.kt
@@ -1,18 +1,51 @@
 import kotlin.math.*
+import java.math.BigInteger
 
-fun isPrime(x:Long): Boolean{
-    if(x==1L) return false
 
-    for (j in 2L..x / 2) {
-            if (x % j == 0L) {
+class BigIntegerRange(
+        override val start: BigInteger,
+        override val endInclusive: BigInteger
+) : ClosedRange<BigInteger>, Iterable<BigInteger> {
+    override operator fun iterator(): Iterator<BigInteger> = BigIntegerRangeIterator(this)
+}
+
+class BigIntegerRangeIterator(
+        private val range: ClosedRange<BigInteger>
+) : Iterator<BigInteger> {
+    private var current = range.start
+
+    override fun hasNext(): Boolean =
+            current <= range.endInclusive
+
+    override fun next(): BigInteger {
+        if (!hasNext()) {
+            throw NoSuchElementException()
+        }
+        return current++
+    }
+}
+
+operator fun BigInteger.rangeTo(other: BigInteger) = BigIntegerRange(this, other)
+
+fun Long.toBigInteger() = BigInteger.valueOf(this)
+fun Int.toBigInteger() = BigInteger.valueOf(toLong())
+
+fun isPrime(x:BigInteger): Boolean{
+    if(x==1L.toBigInteger()) return false
+
+    for (j in 2L.toBigInteger()..x / 2.toBigInteger()) {
+            if (x % j == 0L.toBigInteger()) {
                 return false
             }
     }
     return true
 }
 fun main() {
-    for (i in 1L..16L) {
-        var n = (10.0.pow(i.toDouble()) / 9).toLong()
+    val search_length = 19L
+    var Repunit  = "1"
+    for (i in 1L..search_length) {
+        var n = BigInteger(Repunit)
+        Repunit = Repunit + "1"
         println("%d %b".format(n, isPrime(n)))
     }
 }
